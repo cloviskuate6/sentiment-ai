230510c7edccb0b95033f2817ee0dbe40ed9d1de
kuate choutedjem clovis <cloviskuate6@gmail.com>
2026-06-23 23:43:43 +0200

    update jenkinsfile and fix linting

--- src/main.py
+++ src/main.py
@@ -1,18 +1,22 @@
-# Fichier principal de l'API Sentiment-AI (Commentaire de TP)
+# Fichier principal de l'API Sentiment-AI
 from fastapi import FastAPI
 from src.schemas import PredictionRequest, PredictionResponse
 from src.model import SentimentModel
 
-# 1. Création de l'instance de l'API (ce que cherche Uvicorn)
+
+# 1. Création de l'instance de l'API
 app = FastAPI(title="SentimentAI API")
+
 
 # 2. Chargement du modèle au démarrage
 model = SentimentModel()
+
 
 # Endpoint de Healthcheck
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
 
 # Endpoint de prédiction
 @app.post("/predict", response_model=PredictionResponse)
